49faf50251b1f92d3e8d23b64361e3d09a112a64
partyfowl <partyfowl@users.noreply.github.com>
2022-01-23 22:42:11 +0000

    Updating version dynamically

--- build.sh
+++ build.sh
@@ -3,13 +3,20 @@ set -e
 rm -rf build
 mkdir build
 
+if [[ -z "${GITHUB_RUN_NUMBER}" ]]; then
+    PACKAGE_NAME=goddns-${GITHUB_RUN_NUMBER}.deb
+    sed -i "s/^\(Version:\s*\).*$/\1${GITHUB_RUN_NUMBER}/" packaging/DEBIAN/control
+else
+    PACKAGE_NAME=goddns.deb
+fi
+
 chmod 775 -R packaging/DEBIAN/*inst
 
-cp -r packaging build/goddns
+cp -r packaging build/$PACKAGE_NAME
 
-mkdir -p build/goddns/usr/local/bin/
+mkdir -p build/$PACKAGE_NAME/usr/local/bin/
 go get
-GOOS=linux GOARCH=arm go build -o build/goddns/usr/local/bin/
+GOOS=linux GOARCH=arm go build -o build/$PACKAGE_NAME/usr/local/bin/
 
 cd build
-dpkg-deb --build goddns
+dpkg-deb --build $PACKAGE_NAME
