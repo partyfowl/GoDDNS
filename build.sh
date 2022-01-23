463b3cfb833720bfef6280479c8dc4cb0003212f
partyfowl <partyfowl@users.noreply.github.com>
2022-01-23 20:26:53 +0000

    better build script

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
+set -e
+
 rm -rf build
 mkdir build
 
 cp -r packaging build/goddns
 
 mkdir -p build/goddns/usr/local/bin/
+go get
 GOOS=linux GOARCH=arm go build -o build/goddns/usr/local/bin/
 
 cd build
